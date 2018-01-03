e5b444997d4f2fa884769f3fb955fb6716548c60
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2018-01-03 09:48:18 -0500

    testing OpenMP paralelization

--- tests/pragcc_parallelizer.py
+++ tests/pragcc_parallelizer.py
@@ -26,7 +26,7 @@ SIMPLE_RAW_CODE = """0
 class TestBaseParallelizer(unittest.TestCase):
 
     def setUp(self):
-        self._parallelizer = parallelizer.BaseParallelizer()
+        self._base_parallelizer = parallelizer.BaseParallelizer()
 
     def test_insert_lines_in_raw_code(self):
         """ Test the code insertion function.
@@ -61,7 +61,7 @@ class TestBaseParallelizer(unittest.TestCase):
             ('Second insertion',2)
         ]
 
-        new_raw = self._parallelizer.insert_lines(
+        new_raw = self._base_parallelizer.insert_lines(
             SIMPLE_RAW_CODE,
             insertions
         )
@@ -84,7 +84,7 @@ class TestBaseParallelizer(unittest.TestCase):
             ('Second insertion',-1)
         ]
 
-        new_raw = self._parallelizer.insert_lines(
+        new_raw = self._base_parallelizer.insert_lines(
             SIMPLE_RAW_CODE,
             insertions
         )
@@ -107,7 +107,7 @@ class TestBaseParallelizer(unittest.TestCase):
             ('Second insertion',-2)
         ]
 
-        new_raw = self._parallelizer.insert_lines(
+        new_raw = self._base_parallelizer.insert_lines(
             SIMPLE_RAW_CODE,
             insertions
         )
@@ -126,125 +126,145 @@ class TestBaseParallelizer(unittest.TestCase):
             ('Second insertion',2)
         ]
 
-        new_raw = self._parallelizer.insert_lines(
+        new_raw = self._base_parallelizer.insert_lines(
             empty_raw,
             insertions
         )
 
         self.assertEqual(new_raw,'')
 
+    def test_not_implemented_methods(self):
+        with self.assertRaises(NotImplementedError):
+            self._base_parallelizer.get_raw_pragma(
+                directive_name='parallel',
+                clauses={}
+            )
+            self._base_parallelizer.insert_directives(
+                function_name='some_function',
+                directives={}
+            )
+            self._base_parallelizer.parallelize(metadata=None)
 
 
-# class TestOpenMPObject(unittest.TestCase):
+class TestOpenMP(unittest.TestCase):
 
-#     def setUp(self):
-#         self._basic = os.path.join(TEST_DIR,'basic.c')
-#         self._complex = os.path.join(TEST_DIR,'complex.c')
-#         self._empty = os.path.join(TEST_DIR,'empty.c')
-#         self._bool_type_1 = os.path.join(TEST_DIR,'bool_type_1.c')
-#         self._bool_type_2 = os.path.join(TEST_DIR,'bool_type_2.c')
-#         self._bool_type_3 = os.path.join(TEST_DIR,'bool_type_3.c')
+    def setUp(self):
 
-#         parallel_file_path = os.path.join(TEST_DIR,'parallel.yml')
-#         self._parallel_metadata = metadata.ParallelFile(
-#             file_path=parallel_file_path
-#         )
+        # Set of files to test code annotation with OpenMP directives
+        self._basic = os.path.join(TEST_DIR,'basic.c')
+        self._complex = os.path.join(TEST_DIR,'complex.c')
+        self._empty = os.path.join(TEST_DIR,'empty.c')
+        self._bool_type_1 = os.path.join(TEST_DIR,'bool_type_1.c')
+        self._bool_type_2 = os.path.join(TEST_DIR,'bool_type_2.c')
+        self._bool_type_3 = os.path.join(TEST_DIR,'bool_type_3.c')
 
-#     def tearDown(self):
-#         utils.purge(TEST_DIR,r'^fake_*')
-#         utils.purge(TEST_DIR,r'^omp_*')
-#         utils.purge(TEST_DIR,r'^ccode_*')
-#         utils.purge(TEST_DIR,r'^acc_*')
+        # Loading metadata needed for code annotation with OpenMP directives
+        parallel_file_path = os.path.join(TEST_DIR,'parallel.yml')
 
-#     def test_openmp_object_from_basic_code_file(self):
-#         """
-#             To create a ccode objects , the given code must follow 
-#             the following struture
+        self._parallel_metadata = metadata.ParallelFile(
+            file_path=parallel_file_path
+        )
 
-#             #include <...>
-#             #include <...>
+    def tearDown(self):
 
-#             #define ....
+        # Clean intermediate files created during code annotation
+        utils.purge(TEST_DIR,r'^fake_*')
+        utils.purge(TEST_DIR,r'^omp_*')
+        utils.purge(TEST_DIR,r'^ccode_*')
+        utils.purge(TEST_DIR,r'^acc_*')
 
-            
-#             function_1(){
-        
-#             }
+    def test_openmp_object_from_basic_code_file(self):
+        """
+            To create ccode objects , the given code must follow 
+            the following struture
 
-#             int main(){
-        
-#             }
+            #include <...>
+            #include <...>
 
-#             if the given code do not follow this structure it can 
-#             not be splited in sections, because the code does not
-#             have that sections, then an IndexError takes place.
-#         """
-#         with self.assertRaises(IndexError):
-#             omp = parallelizer.OpenMP(
-#                 file_path=self._basic,
-#                 metadata=self._parallel_metadata
-#             )
-
-#     def test_openmp_object_from_complex_code_file(self):
-#         omp = parallelizer.OpenMP(
-#             file_path=self._complex,
-#             metadata=self._parallel_metadata
-#         )
-
-#         self.assertIsInstance(omp,parallelizer.OpenMP)
-
-#     def test_openmp_object_from_empty_code_file(self):
-#         """
-#             To create a ccode objects , the given code must follow 
-#             the following struture
-
-#             #include <...>
-#             #include <...>
-
-#             #define ....
+            #define ....
 
             
-#             function_1(){
+            function_1(){
         
-#             }
+            }
 
-#             int main(){
+            int main(){
         
-#             }
+            }
 
-#             if the given code do not follow this structure it can 
-#             not be splited in sections, because the code does not
-#             have that sections, then an IndexError takes place.
-#         """
-#         with self.assertRaises(IndexError):
-#             omp = parallelizer.OpenMP(
-#                 file_path=self._empty,
-#                 metadata=self._parallel_metadata
-#             )
+            if the given code do not follow this structure it can 
+            not be splited in sections, because the code does not
+            have that sections, then an IndexError takes place.
+        """
+        with self.assertRaises(IndexError):
+            omp = parallelizer.OpenMP(file_path=self._basic)
 
-#     def test_openmp_object_from_unssupported_type_code_file(self):
-#         with self.assertRaises(ParseError):
-#             omp = parallelizer.OpenMP(
-#                 file_path=self._bool_type_1,
-#                 metadata=self._parallel_metadata
-#             )
+    def test_openmp_object_from_complex_code_file(self):
+        omp = parallelizer.OpenMP(file_path=self._complex)
+        self.assertIsInstance(omp,parallelizer.OpenMP)
 
-#     def test_openmp_object_from_include_bool_type_file(self):
-#         with self.assertRaises(ParseError):
-#             omp = parallelizer.OpenMP(
-#                 file_path=self._bool_type_2,
-#                 metadata=self._parallel_metadata
-#             )
+    def test_openmp_object_from_empty_code_file(self):
+        with self.assertRaises(IndexError):
+            omp = parallelizer.OpenMP(file_path=self._empty)
 
-#     def test_openmp_object_from_include_bool_type_behind_two_includes_file(self):
-#         omp = parallelizer.OpenMP(file_path=self._bool_type_3)
-#         self.assertIsInstance(omp,parallelizer.OpenMP)
+    def test_openmp_object_from_unssupported_type_code_file(self):
+        """ 
+            The C preprocessor used by pycparse for code parsing do not support
+            the bool type natively, if the preprocessor find an unknwon type, the
+            code parsing cann not take place.
+        """
+        with self.assertRaises(ParseError):
+            omp = parallelizer.OpenMP(file_path=self._bool_type_1)
 
-#     def test_openmp_parallelization_from_complex_code_file(self):
-#         omp = parallelizer.OpenMP(
-#             file_path=self._complex,
-#             metadata=self._parallel_metadata
-#         )
+    def test_openmp_object_from_include_bool_type_file(self):
+        """ 
+            We include the stdbool.h header library on which the bool 
+            type is defined. Thus, the C preprocessor will recognize
+            the bool type.
 
-#         ccode = omp.parallelize()
-#         self.assertIsInstance(ccode,code.CCode)
+            But this version of pragcc perform a preprocessing if the
+            code called fake_cfile located in pragcc.core.parser.c99.parser
+            this step remove the first two headers of the original file.
+
+            #include <stdbool.h>
+            #include <stdio.h>
+
+            for fake files
+
+            #include <_fake_defines.h>
+            #include <_fake_typedefs.h>
+
+            So, the bool type would not be recognized again. The follwing 
+            test show a possible solution.
+
+        """
+        with self.assertRaises(ParseError):
+            omp = parallelizer.OpenMP(file_path=self._bool_type_2)
+
+    def test_openmp_object_from_include_bool_type_behind_two_includes_file(self):
+        """
+
+            We place two includes above the stdbool.h
+
+            #include <stdio.h>
+            #include <stdlib.h>
+            #include <stdbool.h>
+
+            So the first two includes are faked out
+
+            #include <_fake_defines.h>
+            #include <_fake_typedefs.h>
+            #include <stdbool.h>
+
+            Then, the bool type will be recognized by the following step,
+            The C preprocessing.          
+
+        """
+        omp = parallelizer.OpenMP(file_path=self._bool_type_3)
+        self.assertIsInstance(omp,parallelizer.OpenMP)
+
+    def test_openmp_parallelization_from_complex_code_file(self):
+        omp = parallelizer.OpenMP(file_path=self._complex)
+
+        ccode = omp.parallelize()
+        self.assertIsInstance(ccode,code.CCode)
